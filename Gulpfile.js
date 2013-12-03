d360b901f40f6d78002070861a26a8639e74818f
Cyrus <david@jcyr.us>
2013-12-03 16:36:45 +0800

    improvements

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,20 +8,28 @@ var g = require('gulp'),
     server = lr();
 
 g.task('less:dev', function () {
-  g.src(pkg.dir.source + '/less/**/*.less')
+  g.src(pkg.dir.source + '/less/style.less')
     .pipe(less())
     .pipe(g.dest(pkg.dir.dest + '/css'))
     .pipe(refresh(server));
 });
 
-g.task('copy:assets', function() {
+g.task('copy:assets', function () {
   g.src(pkg.dir.source + '/{img,font,js}')
     .pipe(g.dest(pkg.dir.dest));
 });
 
+g.task('copy:vendorScripts', function () {
+  [
+    'vendor/jquery/jquery.min.js',
+    'vendor/bootstrap/dist/js/bootstrap.min.js'
+  ].forEach(function (file) {
+    g.src(file).pipe(g.dest(pkg.dir.dest+'/js/vendor'));
+  });
+});
+
 g.task('dot', function () {
   var settings = {
-    templateSettings: null,
     def: pkg.dir.source + '/partials/*.def'
   };
 
@@ -31,24 +39,30 @@ g.task('dot', function () {
     .pipe(refresh(server));
 });
 
-g.task('default', function () {
-  rm(pkg.dir.dest, function (err) {
-    if (err) return console.log(err);
+g.task('clean:dest', function () {
+  rm.sync(pkg.dir.dest);
+});
 
+g.task('default', [
+    'clean:dest',
+    'dot',
+    'less:dev',
+    'copy:assets',
+    'copy:vendorScripts'
+  ], function () {
     server.listen(35729, function (err) {
       if (err) return console.log(err);
 
-      g.run('dot', 'less:dev', 'copy:assets', function () {
-        g.watch(pkg.dir.source + '/{img,font,js}/**/*', function () {
-          g.run('copy:assets');
-        });
-        g.watch(pkg.dir.source + '/less/**/*.less', function () {
-          g.run('less:dev');
-        });
-        g.watch(pkg.dir.source + '/**/*.{dot,def}', function () {
-          g.run('dot');
-        });
+      g.watch(pkg.dir.source + '/{img,font,js}/**/*', function () {
+        g.run('copy:assets');
       });
+      g.watch(pkg.dir.source + '/less/**/*.less', function () {
+        g.run('less:dev');
+      });
+      g.watch(pkg.dir.source + '/**/*.{dot,def}', function () {
+        g.run('dot');
+      });
+
     });
-  });
-});
+  }
+);
